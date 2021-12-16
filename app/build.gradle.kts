d415dd733d7e45d1c383f9dd6a61c7daa29760e3
Rhys Davies <rhys@memes.nz>
2021-12-16 23:18:43 +1300

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,8 +62,7 @@ android {
 }
 
 dependencies {
-    val roomVersion = "2.3.0"
-    val pagingVersion = "3.0.1"
+    val roomVersion = "2.4.0"
 
 
     implementation("androidx.room:room-runtime:$roomVersion")
@@ -72,7 +71,7 @@ dependencies {
 
     implementation("com.jakewharton.timber:timber:5.0.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.31")
-    implementation("androidx.paging:paging-runtime-ktx:$pagingVersion")
+    implementation("androidx.paging:paging-runtime-ktx:3.1.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("androidx.paging:paging-compose:1.0.0-alpha14")
@@ -82,12 +81,12 @@ dependencies {
     implementation("com.google.firebase:firebase-messaging-ktx")
     implementation("com.google.firebase:firebase-perf-ktx")
     implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.appcompat:appcompat:1.4.0")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.compose.ui:ui:1.0.4")
-    implementation("androidx.compose.material:material:1.0.4")
-    implementation("androidx.compose.material:material-icons-extended:1.0.4")
-    implementation("androidx.compose.ui:ui-tooling:1.0.4")
+    implementation("androidx.compose.ui:ui:1.0.5")
+    implementation("androidx.compose.material:material:1.0.5")
+    implementation("androidx.compose.material:material-icons-extended:1.0.5")
+    implementation("androidx.compose.ui:ui-tooling:1.0.5")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
     implementation("androidx.activity:activity-compose:1.4.0")
     implementation("io.coil-kt:coil-compose:1.4.0")
@@ -95,14 +94,14 @@ dependencies {
     implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
-    implementation("androidx.navigation:navigation-compose:2.4.0-beta01")
+    implementation("androidx.navigation:navigation-compose:2.4.0-rc01")
     implementation("com.google.dagger:hilt-android:2.40")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0-alpha03")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0-rc01")
     kapt("com.google.dagger:hilt-android-compiler:2.40")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.4")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.5")
 }
 ktlint {
     android.set(true)
