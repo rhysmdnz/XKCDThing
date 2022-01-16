236d07eba39ff7c88de7aa2daf7a0895fe82a74b
Rhys Davies <rhys@memes.nz>
2022-01-16 19:38:28 +1300

    Update kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.0.4"
+        kotlinCompilerExtensionVersion = "1.2.0-alpha01"
     }
 }
 
@@ -81,10 +81,10 @@ dependencies {
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.4.0")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.compose.ui:ui:1.0.5")
-    implementation("androidx.compose.material:material:1.0.5")
-    implementation("androidx.compose.material:material-icons-extended:1.0.5")
-    implementation("androidx.compose.ui:ui-tooling:1.0.5")
+    implementation("androidx.compose.ui:ui:1.2.0-alpha01")
+    implementation("androidx.compose.material:material:1.2.0-alpha01")
+    implementation("androidx.compose.material:material-icons-extended:1.2.0-alpha01")
+    implementation("androidx.compose.ui:ui-tooling:1.2.0-alpha01")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
     implementation("androidx.activity:activity-compose:1.4.0")
     implementation("io.coil-kt:coil-compose:1.4.0")
