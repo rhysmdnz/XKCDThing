2f501ddc3795f82e5666c0fe3b061d6770495a93
Rhys Davies <rhys@memes.nz>
2022-01-31 23:29:17 +1300

    Update build tools

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         }
     }
     compileSdk = 31
-    buildToolsVersion = "30.0.3"
+    buildToolsVersion = "31.0.0"
 
     defaultConfig {
         applicationId = "nz.memes.xkcdthing"
