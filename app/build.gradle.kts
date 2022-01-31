21c0ad77b6265e52d1675fc3d5872e3ad8f458e3
Rhys Davies <rhys@memes.nz>
2022-01-31 23:31:22 +1300

    Kotlin compiler thing update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.2.0-alpha01"
+        kotlinCompilerExtensionVersion = "1.2.0-alpha02"
     }
 }
 
