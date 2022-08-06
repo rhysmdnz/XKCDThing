190b6a3c966708ee0b1edd2e85f16773cffe553c
Rhys Davies <rhys@memes.nz>
2022-08-06 20:10:32 +1200

    Updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,12 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.1")
+        classpath("com.android.tools.build:gradle:7.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.42")
-        classpath("com.google.gms:google-services:4.3.10")
+        classpath("com.google.gms:google-services:4.3.13")
         classpath("com.google.firebase:perf-plugin:1.4.1")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.0")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.1")
         classpath("com.github.ben-manes:gradle-versions-plugin:0.42.0")
 
         // NOTE: Do not place your application dependencies here; they belong
