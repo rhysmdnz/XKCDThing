406569e4c38255d26dcfb46ec1e9554c40cb59df
Rhys Davies <rhys@memes.nz>
2022-09-19 23:56:10 +1200

    Even more updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.2.0-beta03"
+        kotlinCompilerExtensionVersion = "1.3.1"
     }
     namespace = "nz.memes.xkcdthing"
 }
@@ -98,7 +98,7 @@ dependencies {
     implementation("com.google.accompanist:accompanist-pager:0.23.1")
     implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation("androidx.navigation:navigation-compose:2.5.2")
     implementation("com.google.dagger:hilt-android:2.43.2")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
